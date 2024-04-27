7e952ebb90666f2a5dd921a54ee9a8eb4af89fb2
Ruslan Akhtariev <rakhtariev@icloud.com>
2024-04-26 19:15:13 -0700

    change routes and weights

--- src/types/types.sol
+++ src/types/types.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.20;
 struct Route {
     address tokenIn;
     address tokenOut;
-    uint256 amountIn;
-    uint256 amountOutMinimum;
+    uint8 weightIn;
+    uint8 weightOutMin;
     // uint24 fee; // TODO: consider using parameterized fee
 }
 
